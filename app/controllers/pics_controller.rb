8fcad58dad3912edbcba1a09ba2a0d46b645bdae
ka909185 <kaku.ogi@gmail.com>
2017-04-30 13:16:53 +0900

    image_upload

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -50,7 +50,7 @@ class PicsController < ApplicationController
 
   private
     def pics_params
-      params.require(:pic).permit(:title, :content)
+      params.require(:pic).permit(:title, :content, :picture, :picture_cache)
     end
     def set_pic
       @pic = Pic.find(params[:id])
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,4 +1,5 @@
 class Pic < ActiveRecord::Base
+  mount_uploader :picture, ImageUploader
   validates :title, presence: true
   validates :content, presence: true
   belongs_to :user
